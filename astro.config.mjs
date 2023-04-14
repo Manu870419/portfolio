378ed80dbe7f077c7a67e4b2cbd113ba1e60ddb2
Manu870419 <beghinmanu870419@gmail.com>
2023-04-14 17:14:58 +0200

    MAJ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://Manu870419.github.io',
-    base: '/portfolio',
-});
+export default defineConfig({});
